0267f688c90b95d4e7973b399d0dc66cd086c4a0
gaspar <gasparbarancelli@gmail.com>
2023-04-20 13:58:16 -0300

    Listagem de pods

--- kubernetes/src/main/java/com/fluytcloud/kubernetes/transport/response/PodResponseList.java
+++ kubernetes/src/main/java/com/fluytcloud/kubernetes/transport/response/PodResponseList.java
@@ -1,4 +1,14 @@
 package com.fluytcloud.kubernetes.transport.response;
 
-public record PodResponseList(String namespace, String name) {
+import java.util.List;
+
+public record PodResponseList(
+        String namespace,
+        String name,
+        Integer restarts,
+        String nodeName,
+        String qosClass,
+        String age,
+        String status,
+        List<PodContainerResponseList> containers) {
 }
